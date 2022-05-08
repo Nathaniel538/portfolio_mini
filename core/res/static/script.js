5ef5e20333ef6399158ca44e47d346913c69280f
Jeswin Santosh <jeswin.santosh@outlook.com>
2022-05-08 12:56:23 +0530

    Updates
    Forms
    Visual contrasts
    
    Co-Authored-By: Nathaniel538 <83531199+Nathaniel538@users.noreply.github.com>

--- core/res/static/script.js
+++ core/res/static/script.js
@@ -22,18 +22,26 @@ for (var i=0; themeDots.length > i; i++){
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = 'static/default.css'
+		document.getElementById('a1').style = "color:black;text-align: center;line-height: 0;"
+		document.getElementById('a2').style = "color:black;"
 	}
 
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'static/blue.css'
+		document.getElementById('a1').style = "color:white;text-align: center;line-height: 0;"
+		document.getElementById('a2').style = "color:white;"
 	}
 
 	if(mode == 'green'){
 		document.getElementById('theme-style').href = 'static/green.css'
+		document.getElementById('a1').style = "color:white;text-align: center;line-height: 0;"
+		document.getElementById('a2').style = "color:white;"
 	}
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = 'static/purple.css'
+		document.getElementById('a1').style = "color:white;text-align: center;line-height: 0;"
+		document.getElementById('a2').style = "color:white;"
 	}
 
 	localStorage.setItem('theme', mode)
