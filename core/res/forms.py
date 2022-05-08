842daf09854af38a93320cff02ab52c3d2cac20e
Jeswin Santosh <jeswin.santosh@outlook.com>
2022-05-08 12:45:21 +0530

    Updates
    Co-Authored-By: Nathaniel538 <83531199+Nathaniel538@users.noreply.github.com>

--- core/res/forms.py
+++ core/res/forms.py
@@ -1,7 +1,23 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Contact
 
 class ContactForm(ModelForm):
 	class Meta:
 		model = Contact
 		fields = '__all__'
+		widgets = {
+            'message': Textarea()
+        }
+		labels = {
+            'first_name': 'Name',
+			'subject': 'Subject',
+			'email': 'Email',
+			'message': 'Message'
+        }
+
+	def __init__(self, *args, **kwargs):
+		super(ContactForm, self).__init__(*args, **kwargs)
+		self.fields['first_name'].widget.attrs.update({'class': 'input-field'})
+		self.fields['subject'].widget.attrs.update({'class': 'input-field'})
+		self.fields['email'].widget.attrs.update({'class': 'input-field'})
+		self.fields['message'].widget.attrs.update({'class': 'input-field'})
